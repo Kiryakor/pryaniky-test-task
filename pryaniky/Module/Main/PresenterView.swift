c7d0bad2cafbf9dca7e4877d0c060f8a50d292e1
kiraykorIOS <60466904+kiraykorIOS@users.noreply.github.com>
2020-07-25 10:42:02 +0300

    up

--- pryaniky/Module/Main/PresenterView.swift
+++ pryaniky/Module/Main/PresenterView.swift
@@ -12,10 +12,10 @@ protocol PresenterProtocol {
     init(view:ViewProtocol,router:RouterProtocol)
     func loadData()
     func numberOfRowsInSection() -> Int
+    func didSelectRowAt(indexPath:IndexPath)
     func createCell(indexPath:IndexPath,cell:TableViewCell)
     func checkNotification()
-    func didSelectRowAt(indexPath:IndexPath)
-    func findData(index:Int) -> Int
+    func findDataIndex(index:Int) -> Int
 }
 
 class PresenterView: PresenterProtocol {
@@ -32,9 +32,15 @@ class PresenterView: PresenterProtocol {
     
     func loadData() {
         Networking.loadData(URL: URL(string: "https://pryaniky.com/static/json/sample.json")!) { [weak self] (data,response,error)  in
-            guard let data = data,error == nil else { return }
+            guard let data = data, error == nil else {
+                self?.view.alert(message: "Не удалось подключиться к серверу")
+                return
+            }
             DispatchQueue.main.async {
-                guard let data = try? JSONDecoder().decode(Main.self, from: data) else { return }
+                guard let data = try? JSONDecoder().decode(Main.self, from: data) else {
+                    self?.view.alert(message: "Не удалось декодровать данные")
+                    return
+                }
                 self?.data = data
                 self?.view.reload()
             }
@@ -46,13 +52,13 @@ class PresenterView: PresenterProtocol {
     }
     
     func didSelectRowAt(indexPath:IndexPath) {
-        let index = findData(index: indexPath.row)
+        let index = findDataIndex(index: indexPath.row)
         view.alert(message: (data?.data[index])!.name)
     }
     
     func createCell(indexPath:IndexPath,cell:TableViewCell){
-        let index = findData(index: indexPath.row)
-        ModuleBuilder.createCell(router: router, view: cell, data: (data?.data[index])!) //создание модуля ячейки
+        let index = findDataIndex(index: indexPath.row)
+        ModuleBuilder.createCell(router: router, view: cell, data: (data?.data[index])!)
     }
     
     func checkNotification() {
@@ -65,8 +71,8 @@ class PresenterView: PresenterProtocol {
         }
     }
     
-    func findData(index:Int) -> Int{
-        var indexData:Int = -1 // поиск данных
+    func findDataIndex(index:Int) -> Int{
+        var indexData:Int = -1 
         for i in 0..<(self.data?.data.count)!{
             if self.data?.data[i].name == self.data?.view[index]{
                 indexData = i
